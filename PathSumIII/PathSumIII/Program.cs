afd99851b1d73d3bdc08da8772855c4f1273f5f2
Dinesh Pathak <dineshpthk56@gmail.com>
2020-08-10 16:03:54 +0530

    Added PathSumIII Solution

---
+++ PathSumIII/PathSumIII/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PathSumIII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PathSumIII/PathSumIII/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PathSumIII
+{
+    class Solution
+    {
+        int PathSum_A(TreeNode root, int sum)
+        {
+            if (root == null) return 0;
+            int res = 0;
+            if (root.val == sum) res++;
+            res += PathSum_A(root.left, sum - root.val);
+            res += PathSum_A(root.right, sum - root.val);
+            return res;
+        }
+
+        public int PathSum(TreeNode root, int sum)
+        {
+            if (root == null) return 0;
+            return PathSum(root.left, sum) + PathSum_A(root, sum) + PathSum(root.right, sum);
+        }
+    }
+}
